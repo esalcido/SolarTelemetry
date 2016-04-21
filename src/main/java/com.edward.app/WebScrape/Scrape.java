d74c893e3aeb9aec540331a70388ff8f72cb0d90
esol <edward.salcido@gmail.com>
2016-04-21 16:48:18 -0700

    added code to write data scraped from webpage to a data file.

--- src/main/java/com.edward.app/WebScrape/Scrape.java
+++ src/main/java/com.edward.app/WebScrape/Scrape.java
@@ -17,9 +17,18 @@ public class Scrape {
 
     public static void main(String [] args) throws IOException{
 
+
+
         File html = new File("/home/edwardsalcido/Desktop/wunderground-html.txt");
         FileWriter fw = new FileWriter(html,true);
         BufferedWriter bw = new BufferedWriter(fw);
+        if(!html.exists() ){
+            html.createNewFile();
+        }
+
+        File datahtml = new File("/home/edwardsalcido/Desktop/wunderground-data-html.txt");
+        FileWriter datafw = new FileWriter(datahtml,true);
+        BufferedWriter databw = new BufferedWriter(datafw);
         if(!html.exists() ){
             html.createNewFile();
         }
@@ -29,30 +38,37 @@ public class Scrape {
         writeToFile("https://www.wunderground.com/history/airport/KVNY/2016/04/21/DailyHistory.html?req_city=Valley%20Glen&req_state=CA&reqdb.zip=91401&reqdb.magic=3&reqdb.wmo=99999&MR=1", bw);
 
 
+
         //extract data from the saved file
         File input = html;
+
         Document doc = Jsoup.parse(input,"UTF-8");
 
         Element content = doc.getElementById("historyTable");
         Elements td = content.getElementsByTag("td");
 
         for(Element elmnt: td ){
-           String temperature = elmnt.text();
-            if(temperature.equals("Mean Temperature")){
+           String temperatureName = elmnt.text();
+
+
+            if(temperatureName.equals("Mean Temperature")){
+                System.out.println(temperatureName);
+                datafw.write(temperatureName+ " ");
+
+                String  temperature =  elmnt.nextElementSibling().text();
                 System.out.println(temperature);
-               temperature =  elmnt.nextElementSibling().text();
-                System.out.println(temperature);
+                databw.write(temperature+"\n");
+
+
                 break;
             }
 
         }
 
-        //System.out.println(td);
 
 
-
-
-
+        //close files
+        databw.close();
         bw.close();
 
 
@@ -71,12 +87,9 @@ public class Scrape {
 
 
     public static void scrapeHTML(BufferedWriter bw) throws IOException{
-        Document doc = Jsoup.connect("https://www.wunderground.com/history/airport/KVNY/2016/04/21/DailyHistory.html?req_city=Valley%20Glen&req_state=CA&reqdb.zip=91401&reqdb.magic=3&reqdb.wmo=99999&MR=1").get();
-        Elements temp = doc.select("html");
 
-        String tempS = String.valueOf(temp);
-        System.out.println(temp);
-        bw.write(tempS);
+
+
     }
 
 
